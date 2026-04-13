64936199368418d1019963b72dc2dd02d4e4c23c
ppcasm <ppcasm@gmail.com>
2026-04-13 00:28:45 -0400

    Testing

--- tools/score7/score-toolchain/test.c
+++ tools/score7/score-toolchain/test.c
@@ -1,41 +1,15 @@
-unsigned char ret_uc_0(void) { return 0; }
-unsigned char ret_uc_1(void) { return 1; }
+#include <setjmp.h>
 
-signed char ret_sc_0(void) { return 0; }
-signed char ret_sc_m1(void) { return -1; }
+volatile int x = 0;
+jmp_buf env;
 
-unsigned short ret_us_0(void) { return 0; }
-unsigned short ret_us_1(void) { return 1; }
+int main(void) {
+    int v = setjmp(env);
 
-signed short ret_ss_0(void) { return 0; }
-signed short ret_ss_m1(void) { return -1; }
+    if (v == 0) {
+        x = 123;
+        longjmp(env, 7);
+    }
 
-unsigned int ret_ui_0(void) { return 0; }
-int ret_i_0(void) { return 0; }
-
-long ret_l_0(void) { return 0; }
-unsigned long ret_ul_0(void) { return 0; }
-
-void ret_void(void) { }
-
-unsigned char pass_uc(unsigned char x) { return x; }
-unsigned short pass_us(unsigned short x) { return x; }
-unsigned int pass_ui(unsigned int x) { return x; }
-
-unsigned char local_uc(void)
-{
-    unsigned char x = 0;
-    return x;
-}
-
-unsigned short local_us(void)
-{
-    unsigned short x = 0;
-    return x;
-}
-
-unsigned int local_ui(void)
-{
-    unsigned int x = 0;
-    return x;
+    return (x == 123 && v == 7) ? 0 : 1;
 }
